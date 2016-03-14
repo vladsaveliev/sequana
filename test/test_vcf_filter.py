abdd766b156ad92a089e6ccc71aff129704e7a55
desvillechabrol <d.desvillechabrol@gmail.com>
2016-03-14 11:55:01 +0100

    Use the module filecmp.

--- test/test_vcf_filter.py
+++ test/test_vcf_filter.py
@@ -3,23 +3,16 @@ from . import data
 pathdata = data.__path__[0]
 from easydev import TempFile
 import os
+import filecmp
 
 
 def test_vcf_filter():
     ft = TempFile()
     vcf_output_expected = pathdata + os.sep + "vcf_filter_output.vcf"
     vcf_record = vcf_filter.VCF(pathdata + os.sep + "vcf_filter_input.vcf")
-    filter_dict = {"QUAL": 1000, "FREQ": 90, "INFO": {"DP": ">10",
-                                                      "AO": ">200",
-                                                      "SRP": "<100"}}
+    filter_dict = {"QUAL": 1000, "FREQ": 0.85, "INFO": {"DP": ">10",
+                                                        "AO": ">200",
+                                                        "SRP": "<100"}}
     vcf_record.filter_vcf(filter_dict, ft.name)
-    flag = True
-    with open(ft.name, 'r') as fl1:
-        with open(vcf_output_expected, 'r') as fl2:
-            for line1 in fl1:
-                line2 = fl2.readline()
-                if(line1 != line2):
-                    flag = False
-                    break
-    assert flag == True
+    assert filecmp(ft.name, vcf_output_expected) == True
     ft.delete()
