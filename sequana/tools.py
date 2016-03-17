4eb955274bcb56e20f1059890b265ccd557dbc52
Thomas Cokelaer <cokelaer@gmail.com>
2016-03-17 15:11:03 +0100

    add tools to convert bam to fastq

--- sequana/tools.py
+++ sequana/tools.py
@@ -1,9 +1,21 @@
 import os
+import string
 
 from sequoia import BAM
 
 
 __all__ = ['bam_to_mapped_unmapped_fastq']
+
+
+try:
+    _translate = string.maketrans('ACGTacgt', 'TGCAtgca')
+except:
+    _translate = bytes.maketrans(b'ACGTacgt', b'TGCAtgca')
+
+
+def reverse_complement(seq):
+    # use hastag but for now, do it manually
+    return seq.translate(_translate)[::-1]
 
 def bam_to_mapped_unmapped_fastq(filename, mode='pe'):
     """
@@ -27,42 +39,63 @@ def bam_to_mapped_unmapped_fastq(filename, mode='pe'):
         R1_unmapped = open(newname + "_R1.unmapped.fastq", "bw")
         R2_unmapped = open(newname + "_R2.unmapped.fastq", "bw")
 
-        for i,this in enumerate(bam):
+        
+
+        from easydev import Progress
+        pb = Progress(len(bam))
+        for i, this in enumerate(bam):
             # check that this is paired indeed
             if this.is_paired is False:
-                unpaired +=1
-                # remove it ?
+                stats['unpaired'] +=1
+                # What to do in such case ?
             else:
                 # inpysam, seq is a string and qual a bytes....
-                txt = b"@" + bytes(this.qname, "utf-8") + b"\t%s\n"
-                txt += bytes(this.seq, "utf-8") + b"\n"
-                txt += b"+\n"
-                txt += this.qual + b"\n"
+                if this.is_reverse is True:
+                    #print("reversed complement entry %s" % i)
+                    txt = b"@" + bytes(this.qname, "utf-8") + b"\t%s\n"
+                    revcomp = reverse_complement(this.seq)
+                    txt += bytes(revcomp, "utf-8") + b"\n"
+                    txt += b"+\n"
+                    txt += this.qual[::-1] + b"\n"
+                else:
+                    txt = b"@" + bytes(this.qname, "utf-8") + b"\t%s\n"
+                    txt += bytes(this.seq, "utf-8") + b"\n"
+                    txt += b"+\n"
+                    txt += this.qual + b"\n"
 
+                # Here, we must take keep the pairs so if R1 is mapped
+                # but R2 is unmapped (or the inverse), then the pair is
+                # unmapped
                 if this.is_read1:
-                    if this.is_unmapped:
+                    if this.is_unmapped or this.mate_is_unmapped:
                         R1_unmapped.write(txt % b"1:N:0:GTGAAA")
                         stats['R1_unmapped'] += 1
                     else:
                         R1_mapped.write(txt % b"1:N:0:GTGAAA")
                         stats['R1_mapped'] += 1
                 elif this.is_read2:
-                    if this.is_unmapped:
+                    if this.is_unmapped or this.mate_is_unmapped:
                         R2_unmapped.write(txt % b"2:N:0:GTGAAA")
                         stats['R2_unmapped'] += 1
                     else:
                         R2_mapped.write(txt % b"2:N:0:GTGAAA")
                         stats['R2_mapped'] += 1
+                else:
+                    #pass
+                    print("Neither R1 or R2")
                 if this.is_duplicate:
-                    print(this.qname)
-        print(unpaired)
-        print(stats)
+                    stats['duplicated'] += 1
+            pb.animate(i+1)
+        print('')
+        print("Number of entries in the BAM: %s" % str(i+1))
         R1_mapped.close()
         R2_mapped.close()
         R1_unmapped.close()
         R2_unmapped.close()
+        return stats
     elif mode == "se":
         raise NotImplementedError
     else:
         raise ValueError("mode must be se or pe")
+
 
