7677f447759dbe166c699bbf412e1d536686a39d
Thomas Cokelaer <cokelaer@gmail.com>
2016-03-30 21:56:18 +0200

    add more test related to BAM files

--- test/test_bamtools.py
+++ test/test_bamtools.py
@@ -1,19 +1,47 @@
-from sequana.bamtools import BAM
+from sequana.bamtools import BAM, Alignment, SAMFlags, BAMReport
+from sequana import sequana_data
+from easydev import TempFile
 
-import os
-from . import data
-pathdata = data.__path__[0]
 
 
 def test_bam():
-    s = BAM(pathdata + os.sep + "test.bam")
-    assert len(s) == 432
+    s = BAM(sequana_data("test.bam"))
+    assert len(s) == 1000
 
-    assert len(list(s.iter_unmapped_reads())) == 432
+    assert len(list(s.iter_unmapped_reads())) == 2
     s.reset()
-    assert len(list(s.iter_mapped_reads())) == 0
+    assert len(list(s.iter_mapped_reads())) == 998
     s.reset()
 
-    assert s.get_read_names()[0] == 'HISEQ:426:C5T65ACXX:5:2302:1943:2127'
+    assert s.get_read_names()
 
     s.get_stats()
+
+    with TempFile(suffix='.png') as fh:
+        s.plot_bar_flags(filename=fh.name, logy=True)
+        s.plot_bar_flags(filename=fh.name)
+    
+
+    with TempFile(suffix='.png') as fh:
+        s.plot_bar_mapq(filename=fh.name)
+  
+
+def test_alignment(): 
+    s = BAM(sequana_data("test.bam"))
+    a = Alignment(next(s))
+    a.as_dict()
+
+
+def test_samflags():
+
+    sf = SAMFlags(4095)
+    print(sf)
+    sf.get_meaning()
+    sf.get_flags()
+
+
+def test_bamreport():
+    b = BAM(sequana_data("test.bam"))
+    r = BAMReport()
+    r.set_data(b)
+    r.create_report()
