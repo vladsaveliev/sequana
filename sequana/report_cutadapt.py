f5a907079047b65ca76c4c63572f0642b9403b91
Thomas Cokelaer <cokelaer@gmail.com>
2016-04-20 20:26:40 +0200

    implement the single-end case

--- sequana/report_cutadapt.py
+++ sequana/report_cutadapt.py
@@ -1,15 +1,20 @@
 import easydev
 import os
-
+import io
 
 from reports import HTMLTable
 from .report_adapter_removal import AdapterRemovalReport
+from .report_main import BaseReport
+
+
+import pandas as pd
+
 
 
 class CutAdaptReport(AdapterRemovalReport):
 
-    def __init__(self, input_filename, 
-        output_filename="cutadapt.html", directory="report", **kargs):
+    def __init__(self, output_filename="cutadapt.html",
+                 directory="report", **kargs):
         """.. rubric:: Constructor
 
         :param input_filename: the input data with results of a cutadapt run
@@ -17,13 +22,14 @@ class CutAdaptReport(AdapterRemovalReport):
         :param report: name of the directory where will be saved the HTML files
 
         """
-        super(CutAdaptReport, self).__init__(output_filename=output_filename, 
+        super(CutAdaptReport, self).__init__(
+            output_filename=output_filename,
             directory=directory, **kargs)
-
         self.jinja['title'] = "CutAdapt Report Summary"
 
-        self.input_filename = input_filename
-        with open(input_filename, "r") as fin:
+    def read_data(self, filename):
+        self.input_filename = filename
+        with open(self.input_filename, "r") as fin:
             self._rawdata = fin.read()
 
             if "Total read pairs processed" in self._rawdata:
@@ -78,11 +84,12 @@ class CutAdaptReport(AdapterRemovalReport):
                         self.jinja[key+'_percent'] = "?"
 
             dd = {}
-            for this in data:
+            positions = []
+            for pos, this in enumerate(data):
                 if this.startswith("Command line parameters: "):
                     cmd = this.split("Command line parameters: ")[1]
                     self.jinja['command'] = "cutadapt " + cmd
-                
+
                 if this.startswith("=== ") and "Adapter" in this:
                     name = this.split("=== ")[1].split(" ===")[0].strip()
                     dd['name'] = name
@@ -98,5 +105,63 @@ class CutAdaptReport(AdapterRemovalReport):
                     }
                     adapters.append(dd.copy())
 
+        self.data = {}
         self.data['adapters'] = adapters
+
+        #logs section
+        self.jinja['logs'] = "<pre>\n"+ open(self.input_filename).read() + "</pre>\n"
+
+
+    def get_histogram_data(self):
+        """In cutadapt logs, an adapter section contains 
+        an histogram of matches that starts with a header
+        and ends with a blank line
+        """
+        header = 'length\tcount\texpect\tmax.err\terror counts\n'
+        with open(self.input_filename, 'r') as fin:
+            # not too large so let us read everything
+            data = fin.readlines()
+            scanning_histogram = False
+            adapters = []
+            current_hist = header
+            dfs = {}
+
+            for this in data:
+                # while we have not found a new adapter histogram section, 
+                # we keep going
+                if this.startswith("==="):
+                    if 'read: Adapter' in this:
+                        name = this.split("read:")[1].split(" ===")[0]
+                        name = name.strip()
+                    elif "=== Adapter" in this:
+                        name = this.split("=== ")[1].split(" ===")[0]
+                        name = name.strip()
+                    else:
+                        pass
+
+                if scanning_histogram is False:
+                    if this == header:
+                        # we found the beginning of an histogram
+                        scanning_histogram = True
+                    else:
+                        # we are somewhere in the log we do not care about
+                        pass
+                elif scanning_histogram is True and len(this.strip()) != 0:
+                    # accumulate the histogram data
+                    current_hist += this
+                elif scanning_histogram is True and len(this.strip()) == 0:
+                    # we found the end of the histogram
+                    # save all data 
+                    self.dd = current_hist
+                    df = pd.read_csv(io.StringIO(current_hist), sep='\t')
+                    
+                    dfs[name] = df.set_index("length")
+                    #reinitiate the variables
+                    current_hist = header
+                    scanning_histogram = False
+                else:
+                    pass
+
+        return dfs
+
 
