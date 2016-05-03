2abeeb0012a8b78dcca8e2883d27a4907f5f5b37
desvillechabrol <d.desvillechabrol@gmail.com>
2016-05-03 17:33:50 +0200

    Update report_submapping

--- sequana/report_submapping.py
+++ sequana/report_submapping.py
@@ -8,13 +8,16 @@ from sequana.report_main import BaseReport
 class SubMappingReport(BaseReport):
     """
     """
-    def __init__(self, start, stop, low_threshold=-3, high_threshold=3, 
-            directory="report", output_filename="submapping.html", **kargs):
+    def __init__(self, start, stop, low_threshold, high_threshold, 
+            low_df, high_df, directory="report", 
+            output_filename="submapping.html", **kargs):
         super(SubMappingReport, self).__init__(
                 jinja_filename="submapping/index.html",
                 directory=directory,
                 output_filename=output_filename, **kargs)
         self.jinja['title'] = "Mapping Report [{0},{1}]".format(start, stop)
+        self.low_df = low_df
+        self.high_df = high_df
         self.start = start
         self.stop = stop
         self.low_t = low_threshold
@@ -23,6 +26,9 @@ class SubMappingReport(BaseReport):
     def set_data(self, data):
         self.mapping = data
 
+    def _get_region(self, df):
+        return df[(df["stop"] > self.start) & (df["start"] < self.stop)]
+
     def parse(self):
         self.mapping.write_csv(self.directory + os.sep +
                 "mapping_{0}_{1}.csv".format(self.start, self.stop), 
@@ -30,18 +36,14 @@ class SubMappingReport(BaseReport):
         self.jinja['input_df'] = "'mapping_{0}_{1}.csv'".format(self.start, 
                 self.stop)
 
-        low_cov_df = self.mapping.get_low_coverage(threshold=self.low_t, 
-                start=self.start, stop=self.stop)
-        merge_low_cov = self.mapping.merge_region(low_cov_df)
+        merge_low_cov = self._get_region(self.low_df)
         self.jinja["low_cov_threshold"] = self.low_t
         self.jinja["nb_low_region"] = len(merge_low_cov)
         html = HTMLTable(merge_low_cov)
         html.add_bgcolor("size")
         self.jinja['low_coverage'] = html.to_html(index=False)
         
-        high_cov_df = self.mapping.get_high_coverage(threshold=self.high_t, 
-                start=self.start, stop=self.stop)
-        merge_high_cov = self.mapping.merge_region(high_cov_df)
+        merge_high_cov = self._get_region(self.high_df)
         self.jinja["high_cov_threshold"] = self.high_t
         self.jinja["nb_high_region"] = len(merge_high_cov)
         html = HTMLTable(merge_high_cov)
