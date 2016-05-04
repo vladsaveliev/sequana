2315497a50885fd44e439957a43c7e898dae3fc5
desvillechabrol <d.desvillechabrol@gmail.com>
2016-05-04 14:55:18 +0200

    Update test

--- test/test_vcf_to_snpeff.py
+++ test/test_vcf_to_snpeff.py
@@ -14,9 +14,8 @@ def test_vcf_to_snpeff():
     mydata = vcf_to_snpeff.VcfToSnpeff(
             vcf_filename=pathdata + os.sep + "test.vcf",
             reference=pathdata + os.sep + "test.gb")
-    mydata.add_custom_db()
     mydata = vcf_to_snpeff.VcfToSnpeff(
             vcf_filename=pathdata + os.sep + "test.vcf",
             reference="Bordetella_pertussis_Tohama_I_uid57617")
-    with TempFile(suffix='.png') as fh:
+    with TempFile() as fh:
         mydata.launch_snpeff(fh.name)
