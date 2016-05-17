60d90abb03a3c5ecc5d0d9908af00d67de86a4fe
Thomas Cokelaer <cokelaer@gmail.com>
2016-05-17 21:50:50 +0200

    fixing rtd with dependencies

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     # package installation
     packages = packages,
 
-    install_requires = ["easydev>=0.9.18", "reports>=0.1.8", "matplotlib",
+    install_requires = ["easydev>=0.9.21", "reports>=0.1.8", "matplotlib",
         "pandas", "cutadapt>=1.9.1", "pysam", "pyVCF"],
 
     # here below '': pattern means include that pattern in all packages
