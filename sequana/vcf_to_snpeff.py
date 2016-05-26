1f8ac7ef8f8ad0091cca19e09ae145954694e353
desvillechabrol <d.desvillechabrol@gmail.com>
2016-05-26 17:59:02 +0200

    Improve file check

--- sequana/vcf_to_snpeff.py
+++ sequana/vcf_to_snpeff.py
@@ -33,26 +33,30 @@ class VCFToSnpeff(object):
         self.ref_name = reference.split("/")[-1]
         self.file_format = file_format
         self.ext = {"-genbank": ".gbk", "-gff3": ".gff", "-gtf22": ".gtf"}
-        
+        # Check if snpEff.config is present
         if not os.path.exists("snpEff.config"):
             self._get_snpeff_config()
-        
-        if file_format == "auto":
-            if self._check_database(self.ref_name):
-                if not os.path.exists("data" + os.sep + self.ref_name):
-                    snpeff_dl = sp.Popen(["snpEff", "download", self.ref_name])
-                    snpeff_dl.wait()
-            else:
-                try:
-                    self._check_format(reference)
-                    self._add_custom_db(stdout, stderr)
-                except IOError:
-                    print("The file " + self.ref_name + " is not present in" 
-                        "the directory.\n")
-                    print("And your reference is not present in the database "
-                          "of sequana. If you are sure that the reference is "
-                          "present in the last update of the snpEff database. " 
-                          "Please, import your snpEff.config.\n")
+        # Check if reference is a file
+        if os.path.exists(reference):
+            if not os.path.exists("data" + os.sep + self.ref_name + os.sep +
+                        "snpEffectPredictor.bin"):
+                # Build snpEff predictor
+                self._check_format(reference)
+                self._add_custom_db(stdout, stderr)
+        # Check if reference is present in snpEff database
+        elif self._check_database(self.ref_name):
+            if not os.path.exists("data" + os.sep + self.ref_name):
+                # Download file
+                snpeff_dl = sp.Popen(["snpEff", "download", self.ref_name])
+                snpeff_dl.wait()
+        # If reference is nowhere
+        else:
+            print("The file " + self.ref_name + " is not present in the "
+                  "directory.\n")
+            print("And your reference is not present in the database of "
+                  "sequana. If you are sure that the reference is present in "
+                  "the last update of the snpEff database. Please, import your "
+                  "snpEff.config.\n")
 
     def _check_format(self, reference):
         with open(reference, "r") as fp:
@@ -98,13 +102,16 @@ class VCFToSnpeff(object):
         
         try:
             with open(stdout, "wb") as out, open(stderr, "wb") as err:
-                snp_build = sp.Popen(["snpEff", "build", self.file_format, 
+                snp_build = sp.Popen(["snpEff", "build", self.file_format,
                     self.ref_name], stderr=err, stdout=out)
         except TypeError:
             snp_build = sp.Popen(["snpEff", "build", self.file_format, 
                 self.ref_name], stderr=None, stdout=None)
         snp_build.wait()
-
+        rc = snp_build.returncode()
+        if rc != 0:
+            print("snpEff build return a non-zero code")
+            sys.exit(rc)
 
     def launch_snpeff(self, vcf_filename, output, stderr="annot.err"):
         """ Launch snpEff
