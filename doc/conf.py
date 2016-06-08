f161498b9666a8240a8b9c6c634056c5a70382c8
Thomas Cokelaer <cokelaer@gmail.com>
2016-06-08 15:21:26 +0200

    update doc

--- doc/conf.py
+++ doc/conf.py
@@ -355,4 +355,6 @@ man_pages = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+intersphinx_mapping = {
+    "python": ('http://docs.python.org/', None),
+}
