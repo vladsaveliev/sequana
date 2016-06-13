2a7d58972a9e7b752163563fbcdb041718b2283d
Thomas Cokelaer <cokelaer@gmail.com>
2016-06-13 15:47:08 +0200

    update test

--- test/test_kraken.py
+++ test/test_kraken.py
@@ -1,18 +1,8 @@
-from sequana import KronaMerger, KrakenResults, KrakenTaxon, KrakenBuilder
+from sequana import  KrakenResults, KrakenAnalysis
 from sequana import sequana_data
 from easydev import TempFile, execute
 from nose.plugins.attrib import attr
 
-def test_krona_merger():
-
-    k1 = KronaMerger(sequana_data("test_krona_k1.tsv", "testing"))
-    k2 = KronaMerger(sequana_data("test_krona_k2.tsv", "testing"))
-    k1 += k2
- 
-    with TempFile(suffix='.tsv') as fh:
-        df = k1.to_tsv(fh.name) 
-    assert all(df['count'] == [14043,591,184,132])
-    assert k1['Bacteria\tProteobacteria\tspecies1\n'] == 14043
 
 
 @attr('skip')
@@ -28,10 +18,10 @@ def test_kraken_taxon():
     file1 = sequana_data("Hm2_GTGAAA_L005_R1_001.fastq.gz", "testing")
     file2 = sequana_data("Hm2_GTGAAA_L005_R2_001.fastq.gz", "testing")
 
-    kt = KrakenTaxon([file1, file2], database="%s/kraken_toydb" % tmpdir)
+    kt = KrakenAnalysis([file1, file2], database="%s/kraken_toydb" % tmpdir)
     kt.run()
 
-    kt = KrakenTaxon(file2, database="%s/kraken_toydb" % tmpdir)
+    kt = KrakenAnalysis(file2, database="%s/kraken_toydb" % tmpdir)
     kt.run()
 
     #Clean the kraken db
