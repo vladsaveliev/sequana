a77e76770188461cf7c9007204ea3dbec079dc52
Thomas Cokelaer <cokelaer@gmail.com>
2016-07-20 13:45:15 +0200

    add more tests

--- test/test_tools.py
+++ test/test_tools.py
@@ -1,7 +1,30 @@
-from sequana.tools import bam_to_mapped_unmapped_fastq
+from sequana.tools import bam_to_mapped_unmapped_fastq, reverse_complement, StatsBAM2Mapped
 from sequana import sequana_data
+from sequana.tools import bam_get_paired_distance
+
+
+def test_StatsBAM2Mapped():
+    data = sequana_data("test.bam", "testing")
+    res = StatsBAM2Mapped(data)
+    res.to_html()
 
 
 def test_bam2fastq():
     data = sequana_data("test.bam", "testing")
     res = bam_to_mapped_unmapped_fastq(data)
+
+
+
+def test_reverse():
+    assert reverse_complement("AACCGGTTA") == 'TAACCGGTT'
+
+
+def test_distance():
+    data = sequana_data("test.bam", "testing")
+    distances = bam_get_paired_distance(data)
+
+
+def test_gc_content():
+    from sequana.tools import gc_content
+    data = sequana_data('test.fasta', "testing")
+    gc_content("test.fasta", 10)['seq1']
