8b7f0aaaeb213e8e5c0ec9eff02f35e8bfc97a8f
Thomas Cokelaer <cokelaer@gmail.com>
2016-07-27 20:43:03 +0200

    update example with new data set

--- examples/plot_coverage.py
+++ examples/plot_coverage.py
@@ -9,15 +9,9 @@ Coverage module example
 from sequana import GenomeCov
 from sequana import sequana_data
 bedfile = sequana_data("virus.bed", "data")
-reference = sequana_data("pKM2-eGFP.fa", "data")
 ################################################
 # Reading input BED file
 gc = GenomeCov(bedfile)
-
-##############################################
-# Reading reference and computing GC content
-w_gc = 100
-gc.compute_gc_content(reference, w_gc)
 
 
 ##########################################################
