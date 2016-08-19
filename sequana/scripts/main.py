0bea0d00897fe763247844e3408d177108148998
Thomas Cokelaer <cokelaer@gmail.com>
2016-08-19 16:21:53 +0200

    fix bug in the --project/--input combo

--- sequana/scripts/main.py
+++ sequana/scripts/main.py
@@ -100,7 +100,6 @@ class Options(argparse.ArgumentParser):
         If you have lots of paired-end, use a --glob without --project ; This
         will create as many directories as required.
 
-        Note that all files must have _R1_ and _R2_ tag in their names.
 
 AUTHORS: Thomas Cokelaer and Dimitri Devilleschabrol
 Documentation: http://sequana.readthedocs.io
@@ -377,6 +376,11 @@ def main(args=None):
 
     if options.glob:
         ff = FastQFactory(options.glob)
+        if options.verbose:
+            print("Found %s projects/samples " % len(ff.tags))
+        if len(ff.tags) > 1 and options.project:
+            raise ValueError("""--project can be used with --input/--glob if there is only one project. """)
+
         if options.index_mapper:
             if options.adapters is None or options.adapters not in ["Nextera", "PCRFree"]:
                 raise ValueError("When using --index-mapper, you must also " 
@@ -399,7 +403,10 @@ def main(args=None):
             for tag in ff.tags:
                 sa.print("Found %s project" % tag)
                 #if options.project is None:
-                options.project = tag
+                if len(ff.tags) == 1 and options.project is not None:
+                    pass
+                else:
+                    options.project = tag
                 options.file1 = ff.get_file1(tag)
                 options.file2 = ff.get_file2(tag)
                 if options.index_mapper:
