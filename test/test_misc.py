90be48bbf3595ebdbad4a721bc28a0ae83375fca
Thomas Cokelaer <cokelaer@gmail.com>
2016-09-02 12:10:09 +0200

    add test

--- test/test_misc.py
+++ test/test_misc.py
@@ -1,6 +1,11 @@
 from sequana.misc import *
 
 
+def test_textwrap():
+    res = textwrap("test1test2", width=5, indent=0)
+    assert res.split("\n")[0] == "test1"
+    assert res.split("\n")[1] == "test2"
+
 def findpos():
     pass
 
