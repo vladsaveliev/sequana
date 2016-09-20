768c959ed3218d7a43b211e807e42c617b2374ce
Thomas Cokelaer <cokelaer@gmail.com>
2016-09-20 22:45:19 +0200

    update compressor utility and add a test

--- sequana/scripts/compressor.py
+++ sequana/scripts/compressor.py
@@ -27,8 +27,10 @@ import argparse
 from snakemake import shell
 from easydev import TempFile 
 
+from sequana.scripts.tools import SequanaOptions
 
-class Options(argparse.ArgumentParser):
+
+class Options(argparse.ArgumentParser, SequanaOptions):
     def  __init__(self, prog="sequana_compressor"):
         usage = """Welcome to SEQUANA - Fastq compression standalone
 
@@ -57,16 +59,19 @@ Issues: http://github.com/sequana/sequana
         """
         description = """DESCRIPTION:
         """
-
         super(Options, self).__init__(usage=usage, prog=prog,
                 description=description)
 
         # options to fill the config file
-        self.add_argument("--source", dest="_source", type=str,
+        self.add_argument("--source", dest="source", type=str,
             help="""fastq, fastq.gz, fastq.bz2""")
-        self.add_argument("--target", dest="_target", type=str,
+        self.add_argument("--target", dest="target", type=str,
             help="""fastq, fastq.gz, fastq.bz2 """)
-
+        self.add_argument("--recursive", dest="recursive",
+            default=False,
+            action="store_true", help="""recursive search""")
+        self.add_version(self)
+        self.add_quiet(self)
 
 def main(args=None):
 
@@ -81,8 +86,6 @@ def main(args=None):
     else:
        options = user_options.parse_args(args[1:])
 
-    options.recursive = True
-
     # valid codecs:
     valid_combos = [
         ("fastq", "fastq.gz"),
@@ -96,15 +99,18 @@ def main(args=None):
     temp = TempFile(suffix=".yaml")
     fh = open(temp.name, "w")
     fh.write("compressor:\n")
-    fh.write("    source: %s\n" %options._source)
-    fh.write("    target: %s\n" % options._target)
+    fh.write("    source: %s\n" %options.source)
+    fh.write("    target: %s\n" % options.target)
     fh.write("    recursive: %s\n" % options.recursive)
+    fh.write("    verbose: %s\n" % options.verbose)
     fh.close() # essential to close it because snakemake will try to use seek()
-
     from sequana import Module
     rule = Module("compressor").path + os.sep +  "compressor.rules"
 
-    cmd = "snakemake -s %s  --configfile %s -j 4 -p" % (rule, temp.name)
+    if options.verbose:
+        cmd = "snakemake -s %s  --configfile %s -j 4 -p" % (rule, temp.name)
+    else:
+        cmd = "snakemake -s %s  --configfile %s -j 4 -q" % (rule, temp.name)
     shell(cmd)
     temp.delete()
 
---
+++ test/scripts/test_sequana_compressor.py
@@ -0,0 +1,42 @@
+from sequana.scripts import compressor
+from nose.plugins.attrib import attr
+from sequana import sequana_data
+import os
+
+
+def test_compressor():
+    prog = "sequana_compressor"
+    compressor.main(["sequana_compressor", '--version'])
+    try:
+        compressor.main([prog, '--help'])
+        assert False
+    except SystemExit:
+        pass
+    else:
+        raise Exception
+    try:
+        compressor.main([prog])
+        assert False
+    except SystemExit:
+        pass
+    else:
+        raise Exception
+
+
+    # get a fastq.gz in a temp file and process it
+    from easydev import TempFile
+    import shutil
+    filename = sequana_data("test.fastq.gz")
+    fh = TempFile(suffix=".fastq.gz")
+    shutil.copy(filename, fh.name)
+
+    cwd = os.path.abspath(os.curdir)
+    os.chdir("/tmp")
+    compressor.main([prog, "--source", "fastq.gz", "--target", "fastq.bz2",
+"--quiet"])
+    compressor.main([prog, "--source", "fastq.bz2", "--target", "fastq.gz",
+        "--recursive"])
+    fh.delete()
+
+
+
