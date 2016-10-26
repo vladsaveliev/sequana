32ebc9961bb64300bf3d43b4fddb8e3ca1765df4
Thomas Cokelaer <cokelaer@gmail.com>
2016-10-26 09:59:52 +0200

    updare requirements

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
 
     # pillow, sphinx-gallery and numpydoc are  for the doc only
     # mock is for the test only
-    install_requires = ["easydev>=0.9.22", "reports>=0.1.8", "matplotlib",
+    install_requires = ["easydev>=0.9.22", "reports>=0.2.0", "matplotlib",
         "pandas", "cutadapt>=1.9.1", "bioservices>=1.4.14", "biokit>=0.3.2", 
         "pysam", "pyVCF", "docutils", "sphinx-gallery", "mock", "numpydoc", "pillow",
         "blist"],
