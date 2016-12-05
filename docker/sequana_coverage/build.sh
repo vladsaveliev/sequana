028a4163c8881a49907075ddd44f8aacb86c0a69
Thomas Cokelaer <cokelaer@gmail.com>
2016-12-05 20:33:44 +0100

    Fix docker typo in dockerfile

--- docker/sequana_coverage/build.sh
+++ docker/sequana_coverage/build.sh
@@ -1,2 +1,2 @@
-docker  build  -t="sequana_coverage" .
-docker tag sequana_coverage sequana/sequana_coverage
+docker  build  -t="sequana/sequana_coverage" .
+#docker tag sequana_coverage sequana/sequana_coverage
