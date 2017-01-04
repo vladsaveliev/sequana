6ed165d39b65781084c6b19bddb8537625cb5c36
Thomas Cokelaer <cokelaer@gmail.com>
2017-01-04 14:06:27 +0100

    move to test

--- test_datatools.py
+++
@@ -1,9 +0,0 @@
-from sequana import sequana_data
-
-
-def test_sequana_data():
-
-    f1 = sequana_data("Institut_Pasteur.png")
-    f2 = sequana_data("Institut_Pasteur.png", "images")
-    assert f1 == f2
-  
