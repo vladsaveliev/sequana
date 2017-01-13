8ed9f16f16c035ae128c973cc0a5a7f113878278
Thomas Cokelaer <cokelaer@gmail.com>
2017-01-14 00:42:25 +0100

    New improved browser

--- sequana/gui/browser.py
+++ sequana/gui/browser.py
@@ -1,22 +1,68 @@
 # coding: utf-8
-from PyQt5 import QtCore
+from PyQt5 import QtCore, QtGui, Qt, QtWidgets
 from PyQt5.QtWebKitWidgets import QWebView
+from PyQt5 import QtWebKit
+from PyQt5.QtWidgets import QProgressBar, QLineEdit
 
 
-class MyBrowser(QWebView):
-    #closing = QtCore.Signal()
+# potential resources for improvements:
+# https://github.com/ralsina/devicenzo/blob/master/devicenzo.py
 
-    def __init(self):
-        super().__init__()
-        self.loadFinished.connec(self._results_available)
+class MyBrowser(Qt.QMainWindow):
 
-    def _results_available(self, ok):
-        frame = self.page().mainFrame()
-        print(unicode(frame.toHtml()).encode('utf-8'))
+    def __init__(self, url):
+        Qt.QMainWindow.__init__(self)
+        self.sb = self.statusBar()
 
-    def closeEvent(self, event):
-        #print("done")
-        pass
-        #self.closing.emit()
+        # Progress bar
+        self.pbar = QProgressBar()
+        self.pbar.setMaximumWidth(120)
+
+        # Main page
+        self.wb = QWebView(
+            loadProgress=self.pbar.setValue,
+            loadFinished=self.pbar.hide, 
+            loadStarted=self.pbar.show, 
+            titleChanged=self.setWindowTitle)
+        self.setCentralWidget(self.wb)
+
+        # Main menu
+        self.tb = self.addToolBar("Main Toolbar")
+        for a in (Qt.QWebPage.Back, Qt.QWebPage.Forward, Qt.QWebPage.Reload):
+            self.tb.addAction(self.wb.pageAction(a))
+        self.tb.addAction
+
+        self.url = QLineEdit(returnPressed =lambda:self.wb.setUrl(
+            QtCore.QUrl.fromUserInput(self.url.text())))
+        self.tb.addWidget(self.url)
+
+        self.wb.urlChanged.connect(lambda u: self.url.setText(u.toString()))
 
 
+
+        self.wb.statusBarMessage.connect(self.sb.showMessage)
+        self.wb.page().linkHovered.connect(lambda l: self.sb.showMessage(l, 3000))
+
+        # Search bar
+        self.search = QLineEdit(returnPressed = lambda: self.wb.findText(self.search.text()))
+        self.search.show()
+        self.search.hide() # To make ctrl+F effective, need to show/hide ?
+
+        # THe shortcuts
+        self.showSearch = Qt.QShortcut("Ctrl+F", self, 
+            activated= lambda: (self.search.show() , self.search.setFocus()))
+        self.hideSearch = Qt.QShortcut("Esc", self, 
+            activated= lambda: (self.search.hide(), self.wb.setFocus()))
+        self.quit = Qt.QShortcut("Ctrl+Q", self, activated= self.close)
+        self.zoomIn = Qt.QShortcut("Ctrl++", self, 
+            activated= lambda: self.wb.setZoomFactor(self.wb.zoomFactor()+.2))
+        self.zoomOut = Qt.QShortcut("Ctrl+-", self, 
+            activated= lambda: self.wb.setZoomFactor(self.wb.zoomFactor()-.2))
+        self.zoomOne = Qt.QShortcut("Ctrl+=", 
+            self, activated = lambda: self.wb.setZoomFactor(1))
+
+        self.wb.settings().setAttribute(QtWebKit.QWebSettings.PluginsEnabled, True)
+
+        self.sb.addPermanentWidget(self.search)
+        self.sb.addPermanentWidget(self.pbar)
+        self.wb.load(QtCore.QUrl(url))
