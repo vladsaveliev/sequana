1e1b8845ec6823ba9b07b8271a765f3a04c603f7
Dimitri Desvillechabrol <d.desvillechabrol@gmail.com>
2017-01-20 11:42:11 +0100

    update base_module.py

--- sequana/modules_report/base_module.py
+++ sequana/modules_report/base_module.py
@@ -17,35 +17,81 @@
 #
 ##############################################################################
 """Generic module is the parent module of all other module"""
+import os
+import shutil
 import jinja2
 import io
 import base64
 
 from reports import HTMLTable
+from sequana.utils import config
 
 
 class SequanaBaseModule(object):
     """ Generic Module to write HTML reports.
     """
-    def __init__(self,
-                 template,
-                 output_directory="report/"):
+    required_dir = ("css", "js", "images")
+    def __init__(self):
+        self.output_dir = config.output_dir
+        # Initiate jinja template
+        template = config.template_dict[config.template].load()
+        env = jinja2.Environment(
+                loader=jinja2.FileSystemLoader(template.template_dir))
+        self.j_template = env.get_template(template.base_fn)
+        self._init_report()
+
+    def _init_report(self):
+        """ Create the report directory.
         """
+        # Create report directory
+        if os.path.isdir(config.output_dir) is False:
+            os.mkdir(self.output_dir)
+        for directory in self.required_dir:
+            complete_directory = os.sep.join([self.output_dir, directory])
+            if os.path.isdir(complete_directory) is False:
+                os.mkdir(complete_directory)
+
+        # Copy css/js necessary files
+        for filename in config.css_list:
+            target = os.sep.join([self.output_dir, 'css'])
+            if os.path.isfile(target) is False:
+                shutil.copy(filename, target)
+        for filename in config.js_list:
+            target = os.sep.join([self.output_dir, 'js'])
+            if os.path.isfile(target) is False:
+                shutil.copy(filename, target)
+
+
+    def create_html(self):
+        """ Create html with Jinja2.
         """
-        env = jinja2.Environment(loader=jinja2.FileSystemLoader(template))
-        self.j_template = env.get_template("base.html")
+        report_output = self.j_template.render(config=config,
+                                               module=self)
+        with open(config.output_html, "w") as fp:
+            print(report_output, file=fp)
+
+    def create_link(self, name, target):
+        """ Create an html link with name and target.
+        """
+        link = "<a href={0} download={0}>{1}</a>"
+        return link.format(target, name)
 
     def dataframe_to_html_table(self, dataframe, kwargs=dict()):
-        """
+        """ Convert dataframe in html.
         """
         html = HTMLTable(dataframe)
         return html.to_html(**kwargs)
 
-    def create_embed_png(self, plot_function, kwargs=dict(), style=None):
-        """ Take as a plot function as input and create a html embed png image.
+    def create_embedded_png(self, plot_function, input_arg, kwargs=dict(),
+                            style=None):
+        """ Take as a plot function as input and create a html embedded png
+        image. You must set the arguments name for the output to connect
+        buffer.
         """
         buf = io.BytesIO()
-        plot_function(filename=buf, **kwargs)
+        # add buffer as output of the plot function
+        kwargs = dict({input_arg: buf}, **kwargs)
+        plot_function(**kwargs)
         if style:
             html = '<img style="{0}" '.format(style)
         else:
