ea5868214d37f581cf24336cf7fa0ee04babfcb4
Dimitri Desvillechabrol <d.desvillechabrol@gmail.com>
2017-01-24 16:42:04 +0100

    create script to generate all reports

--- sequana/scripts/reports.py
+++ sequana/scripts/reports.py
@@ -21,8 +21,12 @@ import shutil
 import glob
 import sys
 import argparse
+import re
+import json
 
 import pandas as pd
+
+from sequana.utils import config
 
 
 class Options(argparse.ArgumentParser):
@@ -47,18 +51,56 @@ Issues: http://github.com/sequana/sequana
         super().__init__(usage=usage, prog=prog, description=description)
 
         # Options to create all requested report and generated
+        group = self.add_argument_group("Required arguments")
+        group.add_argument(
+            '-d', '--input-directory', type=str, dest='input_dir', help=(
+            "Search for CSV and JSON files in the directory. It creates a "
+            "report directory with a summary.html as main page."))
+        group.add_argument('-i', '--input', type=str, nargs='*',
+                           dest='input_file',
+                           help="All CSV and JSON files produce by sequana")
+        group.add_argument('-o', '--output-directory', type=str,
+                           dest='output_dir', default='report',
+                           help="Name of the output (report) directory")
+        group = self.add_argument_group("General options")
+        group.add_argument('--version', dest='version', action='store_true',
+                           help="Print version of sequana")
+        group.add_argument('-v', '--verbose', dest='verbose',
+                           action='store_true', help="Display logs") 
 
 
 def main(args=None):
     if args is None:
         args = sys.argv[:]
 
-    user_options = Options(prog="sequana")
+    user_options = Options(prog='sequana')
 
     # If --help or no options provided, show the help
     if len(args) == 1:
-        user_options.parse_args(["prog", "--help"])
+        user_options.parse_args(['prog', '--help'])
     else:
         options = user_options.parse_args(args[1:])
-    options.verbose = not options.quiet
+    config.output_dir = options.output_dir
 
+    # list of all target files (csv or json)
+    input_files = [f for p in ('*.csv', '*.json') for f in
+                   glob.glob(os.sep.join([options.input_dir, p]),
+                   recursive=True)]
+
+    for f in input_files:
+        # get tools to retrieve module
+        with open(f, 'r') as fp:
+            if f.endswith('json'):
+                data = json.load(fp)
+                tool = data['tool']
+            else:
+                line = fp.readline()
+                tool = re.findall("sequana_\w+", line)[0]
+                data = f
+        # load and execute module
+        mod = config.module_dict[tool].load()
+        mod(data)
+
+
+if __name__ == '__main__':
+    main()
