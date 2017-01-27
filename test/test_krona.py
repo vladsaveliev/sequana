23629b2e1c98a8a9eee82bd7ff2fc8d48e89889c
Thomas Cokelaer <cokelaer@gmail.com>
2017-01-27 17:00:39 +0100

    use timeout on travis to see where are the errors

--- test/test_krona.py
+++ test/test_krona.py
@@ -1,4 +1,4 @@
-from sequana import KronaMerger 
+from sequana import KronaMerger
 from sequana import sequana_data
 from easydev import TempFile, execute
 
@@ -7,9 +7,9 @@ def test_krona_merger():
     k1 = KronaMerger(sequana_data("test_krona_k1.tsv"))
     k2 = KronaMerger(sequana_data("test_krona_k2.tsv"))
     k1 += k2
- 
+
     with TempFile(suffix='.tsv') as fh:
-        df = k1.to_tsv(fh.name) 
+        df = k1.to_tsv(fh.name)
     assert all(df['count'] == [14043,591,184,132])
     assert k1['Bacteria\tProteobacteria\tspecies1\n'] == 14043
 
