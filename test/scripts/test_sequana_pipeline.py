60a22e66784f5e1abba1eba833e90e94350cf0d5
Thomas Cokelaer <cokelaer@gmail.com>
2017-01-27 16:07:12 +0100

    update tests for travis

--- test/scripts/test_sequana_pipeline.py
+++ test/scripts/test_sequana_pipeline.py
@@ -1,22 +1,11 @@
 from sequana.scripts import main
-from nose.plugins.attrib import attr
-import mock
 from sequana import sequana_data
 import os
 
 
-#@attr('skip')
-class TestPipeline(object):
+prog = "sequana"
 
-    @classmethod
-    def setup_class(klass):
-        """This method is run once for each class before any tests are run"""
-        klass.prog = "sequana"
-        klass.params = {'prog': klass.prog}
-
-    @classmethod
-    def teardown_class(klass):
-        """This method is run once for each class _after_ all tests are run"""
+"""    def teardown_class(klass):
         try:os.remove('quality.rules')
         except:pass
         try:os.remove('config.yaml')
@@ -28,62 +17,58 @@ class TestPipeline(object):
 
         try:shutil.rmtree("report")
         except:pass
+"""
 
-    def test_version(self):
-        main.main([self.prog, '--version'])
+def test_version():
+    main.main([prog, '--version'])
 
-    def test_init(self):
-        try:
-            # py3
-            with mock.patch('builtins.input', return_value="y"):
-                try:
-                    main.main([self.prog, '--pipeline', "qualitydummy"])
-                    assert False
-                except:
-                    assert True
-        except:
-            # py2
-            with mock.patch('__builtin__.input', return_value="y"):
-                main.main([self.prog, '--pipeline', "quality_control"])
+def test_init(mocker):
+    try:
+        # py3
+        with mocker.patch('builtins.input', return_value="y"):
+            try:
+                main.main([prog, '--pipeline', "qualitydummy"])
+                assert False
+            except:
+                assert True
+    except:
+        # py2
+        with mocker.patch('__builtin__.input', return_value="y"):
+            main.main([prog, '--pipeline', "quality_control"])
+        with mocker.patch('__builtin__.input', return_value="y"):
+            try:
+                main.main([prog, '--pipeline', "qualitydummy"])
+                assert False
+            except:
+                assert True
 
-            with mock.patch('__builtin__.input', return_value="y"):
-                try:
-                    main.main([self.prog, '--pipeline', "qualitydummy"])
-                    assert False
-                except:
-                    assert True
-
-    def test_run(self):
+def test_help():
+    try:
+        main.main([prog, '--help'])
+        assert False
+    except SystemExit:
         pass
+    else:
+        raise Exception
 
-    def test_help(self):
-        try:
-            main.main([self.prog, '--help'])
-            assert False
-        except SystemExit:
-            pass
-        else:
-            raise Exception
+def test_info():
+    main.main([prog, '--info', "quality"])
 
-    #@attr('onweb')
-    #def test_info(self):
-    #    main.main([self.prog, '--info', "quality"])
+def test_show_pipelines():
+    main.main([prog, '--show-pipelines'])
 
-    def test_show_pipelines(self):
-        main.main([self.prog, '--show-pipelines'])
+#def test_mutually_exclusive():
+#    try:
+#        main.main([prog, '--pipeline', 'quality', '--info'])
+#        assert False
+#    except:
+#        assert True
 
-    #def test_mutually_exclusive(self):
-    #    try:
-    #        main.main([self.prog, '--pipeline', 'quality', '--info'])
-    #        assert False
-    #    except:
-    #        assert True
-
-    def _test_input(self):
-        file1 = sequana_data('Hm2_GTGAAA_L005_R1_001.fastq.gz', 'data')
-        file2 = sequana_data('Hm2_GTGAAA_L005_R2_001.fastq.gz', 'data')
-        main.main([self.prog, "--pipeline", "quality_control", "--file1", 
-                  file1, "--file2", file2])
+def test_input():
+    file1 = sequana_data('Hm2_GTGAAA_L005_R1_001.fastq.gz', 'data')
+    file2 = sequana_data('Hm2_GTGAAA_L005_R2_001.fastq.gz', 'data')
+    main.main([prog, "--pipeline", "quality_control", "--file1", 
+              file1, "--file2", file2, "--no-adapters" , "--force"])
 
 
 
