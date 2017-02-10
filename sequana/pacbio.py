7734485277c38620e598be9e2392f23ffe586dc4
Thomas Cokelaer <cokelaer@gmail.com>
2017-02-10 15:18:02 +0100

    add module dedicated to pacbio analysis

---
+++ sequana/pacbio.py
@@ -0,0 +1,22 @@
+import pysam
+
+
+class PacBioInputBAM(object):
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.data = pysam.AlignmentFile(filename, check_sq=False)
+
+        
+
+    def reset(self):
+        self.data.close()
+        self.data = pysam.AlignmentFile(self.filename, check_sq=False)
+
+    def stride(self, output_filename, stride=10):
+        with pysam.AlignmentFile(output_filename,  "wb", template=self.data) as fh:
+
+            for i, read in enumerate(self.data):
+                if i % stride == 0: 
+                    fh.write(read)
+
