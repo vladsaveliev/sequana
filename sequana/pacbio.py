c6c84434dba9063056c5269b33792018a3c1f85d
mcardon <melissa.cardon@pasteur.fr>
2017-02-24 17:27:17 +0100

    pacbio QC improvements #355

--- sequana/pacbio.py
+++ sequana/pacbio.py
@@ -1,9 +1,10 @@
 
 from sequana.lazy import pylab
-import collections #lazy ?
 from sequana.lazy import numpy as np
+from sequana.lazy import pandas as pd
+import collections #lazy ?
 import pysam
-
+from biokit.viz import hist2d
 
 class PacBioInputBAM(object):
     """PacBio utilities
@@ -19,19 +20,65 @@ class PacBioInputBAM(object):
         self.filename = filename
         self.data = pysam.AlignmentFile(filename, check_sq=False)
         self._N = None
-        self.GC_percent = None
-        self.all_read_len = None
-        self.nb_pass = None
+        self._df = None
+        self._nb_pass = None
 
     def __len__(self):
         if self._N is None:
-            self.reset()
-            self._N = sum([1 for this in self.data])
-            self.reset()
+            df = self._get_df()
         return self._N
 
     def __str__(self):
         return "Length: {}".format(len(self))
+
+    def _get_df(self):
+        if self._df is None:
+            self.reset()
+            N = 0
+            
+            all_results = []
+            for read in self.data:
+                res = []
+                # count reads
+                N += 1
+                if (N % 10000) == 0:
+                    print("Read %d sequences" %N)
+                #res[0] = read length
+                res.append(read.query_length)
+                # res[1] = GC content
+                c = collections.Counter(read.query_sequence)
+                res.append( (c['g'] + c['G'] + c['c'] + c['C'])/float(sum(c.values())) )
+                # res[2] = snr A
+                # res[3] = snr C
+                # res[4] = snr G
+                # res[5] = snr T
+                snr = list([x for x in read.tags if x[0]=='sn'][0][1])
+                res = res + snr
+                #res[6] = ZMW name
+                res.append(read.qname.split('/')[1])
+                
+                # aggregate results
+                all_results.append(res)
+
+            self._df = pd.DataFrame(all_results, columns=['read_length','GC_content','snr_A','snr_C','snr_G','snr_T','ZMW'])
+            self._N = N
+            self.reset()     
+        return self._df
+
+    df = property(_get_df)
+
+    def _get_ZMW_passes(self):
+        print()
+        if self._nb_pass is None:
+            if self._df is None:
+                self._get_df()
+
+            zmw_passes = collections.Counter(self._df.loc[:,'ZMW'])
+            distrib_nb_passes = [zmw_passes[z] for z in zmw_passes.keys()]
+            self._nb_pass = collections.Counter(distrib_nb_passes)
+        return self._nb_pass
+
+    nb_pass = property(_get_ZMW_passes)
 
     def reset(self):
         self.data.close()
@@ -45,88 +92,84 @@ class PacBioInputBAM(object):
                 if i % stride == 0: 
                     fh.write(read)
 
-    def GC_content(self):
-        """GC content for each read
-        Save result of GC content in self.GC_percent if not already present"""
-        if self.GC_percent is None:
-            self.reset()
-            GC_percent = []
-            for read in self.data:
-                c = collections.Counter(read.query_sequence)
-                GC_percent.append( (c['g'] + c['G'] + c['c'] + c['C'])/float(sum(c.values())) )
-            self.reset()
-            self.GC_percent = GC_percent
-
-    def read_len(self):
-        """Read len for each read)"""
-        if self.all_read_len is None:
-            self.reset()
-            self.all_read_len = [read.query_length for read in self.data]
 
     def hist_snr(self, bins=50, alpha=0.5, hold=False, fontsize=12,
                 grid=True,xlabel="SNR",ylabel="#"):
         """Plot histogram of the ACGT SNRs for all reads"""
-        self.reset()
-        reads = [[x for x in read.tags if x[0]=='sn'] for read in self.data]
-        snr = [x[0][1] for x in reads]
+        if self._df is None:
+            self._get_df()
+
         if hold is False:
             pylab.clf()
-        pylab.hist([x[0] for x in snr], alpha=alpha, label="A", bins=bins)
-        pylab.hist([x[1] for x in snr], alpha=alpha, label="C", bins=bins)
-        pylab.hist([x[2] for x in snr], alpha=alpha, label="G", bins=bins)
-        pylab.hist([x[3] for x in snr], alpha=alpha, label="T", bins=bins)
+        pylab.hist(self._df.loc[:,'snr_A'], alpha=alpha, label="A", bins=bins)
+        pylab.hist(self._df.loc[:,'snr_C'], alpha=alpha, label="C", bins=bins)
+        pylab.hist(self._df.loc[:,'snr_G'], alpha=alpha, label="G", bins=bins)
+        pylab.hist(self._df.loc[:,'snr_T'], alpha=alpha, label="T", bins=bins)
         pylab.legend()
         pylab.xlabel(xlabel, fontsize=fontsize)
         pylab.ylabel(ylabel, fontsize=fontsize)
         if grid is True:
             pylab.grid(True)
 
-    def hist_ZMW_subreads(self):
+    def hist_ZMW_subreads(self, hold=False, fontsize=12,
+                            grid=True,xlabel="Number of ZMW passes",ylabel="#"):
         """
         Plot histogram of number of reads per ZMW
-        Save counter in nb_pass
         """
-        if self.nb_pass is None:
-            self.reset()
-            zmw = [read.qname.split('/')[1] for read in self.data]
-            zmw_passes = collections.Counter(zmw)
-            distrib_nb_passes = [zmw_passes[z] for z in zmw_passes.keys()]
-            max_nb_pass = max(distrib_nb_passes)
-            self.nb_pass = collections.Counter(distrib_nb_passes)
+        if self._nb_pass is None:
+            self._get_ZMW_passes()
 
-        max_nb_pass = max(self.nb_pass.keys())
+        max_nb_pass = max(self._nb_pass.keys())
         k = range(1,max_nb_pass+1)
-        print(k)
-        val = [self.nb_pass[i] for i in k]
-        print(val)
-        # histogram nb passes
-        pylab.hist(k, weights=val, bins=max_nb_pass)
-        pylab.xlabel("Nb passes")
-        pylab.ylabel("ZMW")
-        pylab.yscale('log')
-        pylab.title("Nb passes")
+        val = [self._nb_pass[i] for i in k]
 
-    def hist_GC(self, bins=50):
+        # histogram nb passes
+        if hold is False:
+            pylab.clf()
+        pylab.hist(k, weights=val, bins=max_nb_pass)
+        pylab.xlabel(xlabel, fontsize=fontsize)
+        pylab.ylabel(ylabel, fontsize=fontsize)
+        pylab.yscale('log')
+        pylab.title("Number of ZMW passes",fontsize=fontsize)
+        if grid is True:
+            pylab.grid(True)
+
+    def hist_GC(self, bins=50, hold=False, fontsize=12,
+                grid=True,xlabel="GC %",ylabel="#"):
         """Plot histogram GC content"""
-        self.GC_content()
-        mean_GC =  np.mean(self.GC_percent)
+
+        if self._df is None:
+            self._get_df()
+        mean_GC =  np.mean(self._df.loc[:,'GC_content'])
 
         # histogram GC percent
-        pylab.hist(self.GC_percent, bins=bins)
-        pylab.xlabel("GC percent")
-        pylab.title("GC content  \n Mean GC : %.2f" %(mean_GC))
+        if hold is False:
+            pylab.clf()
+        pylab.hist(self._df.loc[:,'GC_content'], bins=bins)
+        pylab.xlabel(xlabel, fontsize=fontsize)
+        pylab.ylabel(ylabel, fontsize=fontsize)
+        pylab.title("GC %%  \n Mean GC : %.2f" %(mean_GC), fontsize=fontsize)
+        if grid is True:
+            pylab.grid(True)
 
-    def plot_GC_read_len(self, alpha=0.07):
+    def plot_GC_read_len(self, alpha=0.07, hold=False, fontsize=12,
+                grid=True,xlabel="GC %",ylabel="#"):
         """Plot GC content versus read length"""
-        self.read_len()
-        self.GC_content()
-        mean_len =  np.mean(self.all_read_len)
-        mean_GC =  np.mean(self.GC_percent)
 
-        pylab.plot(self.all_read_len , self.GC_percent, 'bo', alpha=alpha)
-        pylab.xlabel("Read length")
-        pylab.ylabel("GC percent")
-        pylab.title("GC content vs length \n Mean length = %.2f , Mean GC : %.2f" %(mean_len, mean_GC))
+        if self._df is None:
+            self._get_df()
+        mean_len =  np.mean(self._df.loc[:,'read_length'])
+        mean_GC =  np.mean(self._df.loc[:,'GC_content'])
+
+        if hold is False:
+            pylab.clf()
+        data = self._df.loc[:,['read_length','GC_content']]
+        h = hist2d.Hist2D(data)
+        res = h.plot(bins=[40,40], contour=False, nnorm='log', Nlevels=6)
+        #pylab.plot(self._df.loc[:,'read_length'] , self._df.loc[:,'GC_content'], 'bo', alpha=alpha)
+        pylab.xlabel("Read length", fontsize=12)
+        pylab.ylabel("GC %", fontsize=12)
+        pylab.title("GC % vs length \n Mean length : %.2f , Mean GC : %.2f" %(mean_len, mean_GC))
 
 
 
