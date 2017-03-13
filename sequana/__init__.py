e4056d9a04af114a4df85ffed9f67cb46abd4b2e
Thomas Cokelaer <cokelaer@gmail.com>
2017-03-13 14:01:44 +0100

    increase version

--- sequana/__init__.py
+++ sequana/__init__.py
@@ -3,7 +3,7 @@ import pkg_resources
 try:
     version = pkg_resources.require("sequana")[0].version
 except:
-    version = ">=0.18.0"
+    version = ">=0.20.0"
 
 import colorlog as logger
 def sequana_debug_level(level="WARNING"):
