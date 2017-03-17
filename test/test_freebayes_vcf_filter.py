3c610ee74621aa844220130a8a00f9e968bdbf78
Dimitri Desvillechabrol <d.desvillechabrol@gmail.com>
2017-03-17 13:26:43 +0100

    update freebayes_vcf_filter

--- test/test_freebayes_vcf_filter.py
+++ test/test_freebayes_vcf_filter.py
@@ -7,11 +7,12 @@ from sequana.freebayes_vcf_filter import VCF_freebayes
 
 def test_vcf_filter():
     vcf_output_expected = sequana_data('JB409847.expected.vcf')
-    vcf_record = VCF_freebayes(sequana_data('JB409847.vcf'))
+    v = VCF_freebayes(sequana_data('JB409847.vcf'))
     filter_dict = {'freebayes_score': 200, 'frequency': 0.85, 'min_depth': 10,
                    'forward_depth': 3, 'reverse_depth': 3, 'strand_ratio': 0.3}
+    filter_v = v.filter_vcf(filter_dict)
     with TempFile(suffix='.vcf') as ft:
-        vcf_record.filter_vcf(filter_dict, ft.name)
+        filter_v.to_vcf(ft.name)
         compare_file = filecmp.cmp(ft.name, vcf_output_expected)
         assert compare_file
 
@@ -21,6 +22,7 @@ def test_constructor():
 def test_to_csv():
     filter_dict = {'freebayes_score': 200, 'frequency': 0.85, 'min_depth': 10,
                    'forward_depth': 3, 'reverse_depth': 3, 'strand_ratio': 0.3}
-    vcf_record = VCF_freebayes(sequana_data('JB409847.expected.vcf'))
+    v = VCF_freebayes(sequana_data('JB409847.expected.vcf'))
+    filter_v = v.filter_vcf(filter_dict)
     with TempFile(suffix='.csv') as ft:
-        vcf_record.to_csv(ft.name, filter_dict)
+        filter_v.to_csv(ft.name)
