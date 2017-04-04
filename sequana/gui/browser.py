5e404ca11d224e06b0a9511b9f63806604e286f3
Thomas Cokelaer <cokelaer@gmail.com>
2017-04-04 13:18:22 +0200

    Fix missing import

--- sequana/gui/browser.py
+++ sequana/gui/browser.py
@@ -7,6 +7,7 @@ try:
     from PyQt5 import QtWebEngine as QtWebKit
     from PyQt5.Qt import QWebEnginePage as QWebPage
     from PyQt5.QtWebEngineWidgets import QWebEngineSettings
+    from PyQt5.Qt import QTabWidget
 except:
     # PyQt 5.6 and below
     from PyQt5.QtWebKitWidgets import QWebView
@@ -18,32 +19,35 @@ from PyQt5.QtWidgets import QProgressBar, QLineEdit
 # potential resources for improvements:
 # https://github.com/ralsina/devicenzo/blob/master/devicenzo.py
 
+
 class Browser(Qt.QMainWindow):
     """
 
-    On purpose, there is no caching so that (if re-generated), the 
+    On purpose, there is no caching so that (if re-generated), the
     new content of an HTML is shown.
 
     """
     def __init__(self, url):
         Qt.QMainWindow.__init__(self)
-        self.sb = self.statusBar()
 
         # Progress bar
         # ------------------------------------------------------------
         self.pbar = QProgressBar()
         self.pbar.setMaximumWidth(120)
 
-        # Main page
+
+        # Main page QWebView
         # -------------------------------------------------------------
         self.wb = QWebView(
             loadProgress=self.pbar.setValue,
-            loadFinished=self.pbar.hide, 
-            loadStarted=self.pbar.show, 
+            loadFinished=self.pbar.hide,
+            loadStarted=self.pbar.show,
             titleChanged=self.setWindowTitle)
+        self.wb.urlChanged.connect(lambda u: self.url.setText(u.toString()))
+
         self.setCentralWidget(self.wb)
 
-        # Main menu
+        # Main menu tool bar
         # -------------------------------------------------------------
         self.tb = self.addToolBar("Main Toolbar")
         for a in (QWebPage.Back, QWebPage.Forward, QWebPage.Reload):
@@ -53,8 +57,8 @@ class Browser(Qt.QMainWindow):
             QtCore.QUrl.fromUserInput(self.url.text())))
         self.tb.addWidget(self.url)
 
-        self.wb.urlChanged.connect(lambda u: self.url.setText(u.toString()))
-
+        # status bar ---------------------------------------------------
+        self.sb = self.statusBar()
         try:
             #pyqt5.6
             self.wb.statusBarMessage.connect(self.sb.showMessage)
@@ -82,7 +86,11 @@ class Browser(Qt.QMainWindow):
         self.zoomOne = Qt.QShortcut("Ctrl+=",
             self, activated = lambda: self.wb.setZoomFactor(1))
 
-        #self.zoomOne = Qt.QShortcut("Ctrl+f", self, activated=lambda: self.wb.back())
+        # Add alt+left and right keys to navigate backward and forward
+        Qt.QShortcut(QtCore.Qt.AltModifier + QtCore.Qt.Key_Left, self,
+            activated=lambda: self.wb.back())
+        Qt.QShortcut(QtCore.Qt.AltModifier + QtCore.Qt.Key_Right, self,
+            activated=lambda: self.wb.forward())
 
         # Javascript and other settings
         # ------------------------------------------------------------
@@ -100,6 +108,8 @@ class Browser(Qt.QMainWindow):
         # Add components on the page
         self.sb.addPermanentWidget(self.search)
         self.sb.addPermanentWidget(self.pbar)
+
+        # Finally, load the URL
         self.wb.load(QtCore.QUrl(url))
 
         # No caching
