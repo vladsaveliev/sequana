bc1f498c4bf2393f456f8a268fbacff206f2a74f
mcardon <melissa.cardon@pasteur.fr>
2017-05-09 17:02:35 +0200

    add histogram to Repeats() and fix bugs

--- sequana/sequence.py
+++ sequana/sequence.py
@@ -5,6 +5,7 @@ from collections import Counter
 
 from sequana.fasta import FastA
 from sequana.lazy import pandas as pd
+from sequana.lazy import pylab
 
 
 class Sequence(object):
@@ -163,7 +164,7 @@ class Repeats(object):
 
 
     """
-    def __init__(self, filename_fasta):
+    def __init__(self, filename_fasta,merge=False):
         """.. rubric:: Constructor
 
         Input must be a fasta file with valid DNA or RNA characters
@@ -172,14 +173,19 @@ class Repeats(object):
          sequence is used.
         :param int threshold: Minimal length of repeat to output
         """
-        self._threshold                 = None
-        self._df_shustring              = None
-        self._header                    = None
-        self._length                    = None
-        self._longest_shustring         = None
-        self._begin_end_repeat_position = None
-        self._filename_fasta            = filename_fasta
-        self._previous_thr              = None
+        self._threshold                       = None
+        self._df_shustring                    = None
+        self._header                          = None
+        self._length                          = None
+        self._longest_shustring               = None
+        self._begin_end_repeat_position       = None
+        self._begin_end_repeat_position_merge = None
+        self._filename_fasta                  = filename_fasta
+        self._previous_thr                    = None
+        self._list_len_repeats                = None
+        if not isinstance(merge, bool):
+            raise TypeError("do_merge must be boolean")
+        self._do_merge                        = merge
 
     def _get_header(self):
         """get first line of fasta (needed in input shustring) 
@@ -240,7 +246,7 @@ class Repeats(object):
     longest_shustring = property(_get_longest_shustring)
 
 
-    def _find_begin_end_repeats(self):
+    def _find_begin_end_repeats(self,force=False):
         """Returns position of repeats longer than threshold
         as an ordered list
         """
@@ -252,7 +258,7 @@ class Repeats(object):
             raise ValueError("threshold : please set threshold (minimum length of repeats to output)")
 
         # if there is no result yet, or the threshold has changed
-        if (self._begin_end_repeat_position is None) | (self.threshold != self._previous_thr):
+        if (self._begin_end_repeat_position is None) | (self.threshold != self._previous_thr) | force:
             nb_row = self.df_shustring.shape[0]
             i = 0
             step_repeat_seq = []
@@ -266,44 +272,20 @@ class Repeats(object):
                 # begining of repeat
                 if (list_len_shus[i] > self.threshold):
                     b = i
-                    # add 0 for non repeat area
-                    step_repeat_seq = step_repeat_seq + [0 for j in range(e,b,1)]
                     # compute new end of repeat
                     len_repeat = list_len_shus[i]
                     e = b + len_repeat
-                    # add len of repeat for all repeat area
-                    step_repeat_seq = step_repeat_seq + [len_repeat for j in range(b,e,1)]
                     # save (b,e)
                     be_repeats.append((b,e))
                     # update i
                     i = e-1
                 i +=1
-            # add 0 at the end if needed
-            step_repeat_seq = step_repeat_seq + [0 for j in range(e,self._length,1)]
 
-            # if there are repeats, merge repeats that are fragmented
-            if len(be_repeats) > 0:
-                prev_tup = be_repeats[0]
-                b = prev_tup[0]
-                begin_end_repeat_position = []
-                for i in range(1,len(be_repeats),1):
-                    tup = be_repeats[i]
-                    if tup[0] == prev_tup[1]:
-                        # concat
-                        e = tup[1]
-                        prev_tup = tup
-                        if i == (len(be_repeats) -1):
-                            # last tup : append to result
-                            begin_end_repeat_position.append((b,e))
-                    else:
-                        # real end of repeat : append result and update b, e
-                        e = prev_tup[1]
-                        begin_end_repeat_position.append((b,e))
-                        prev_tup = tup
-                        b = prev_tup[0]
-            else:
-                begin_end_repeat_position = []
-            self._begin_end_repeat_position = begin_end_repeat_position
+            self._begin_end_repeat_position = be_repeats
+
+        self._get_merge_repeats()
+
+            
 
         #return self._begin_end_repeat_position
     def _get_be_repeats(self):
@@ -312,14 +294,83 @@ class Repeats(object):
 
     begin_end_repeat_position = property(_get_be_repeats)
 
-    def _set_threshold(self,value=0):
+    def _set_threshold(self,value):
+        if value < 0:
+            raise ValueError("Threshold must be positive integer")
         if value != self._threshold:
             self._previous_thr = self._threshold
         self._threshold = value
         self._find_begin_end_repeats()
-        
+        self._list_len_repeats = [tup[1]-tup[0] for tup in self._begin_end_repeat_position]
+
     def _get_threshold(self):
         return self._threshold
 
     threshold = property(_get_threshold, _set_threshold)
+
+    def _get_list_len_repeats(self):
+        if self._list_len_repeats is None:
+            raise UserWarning("Please set threshold (minimum length of repeats to output)")
+        return self._list_len_repeats
+
+    list_len_repeats = property(_get_list_len_repeats)
+
+    def _get_merge_repeats(self):
+        if self._do_merge:
+            # if there are repeats, merge repeats that are fragmented
+            if len(self._begin_end_repeat_position) > 0:
+                prev_tup = self._begin_end_repeat_position[0]
+                b = prev_tup[0]
+                begin_end_repeat_position_merge = []
+                for i in range(1,len(self._begin_end_repeat_position),1):
+                    tup = self._begin_end_repeat_position[i]
+                    if tup[0] == prev_tup[1]:
+                        # concat
+                        e = tup[1]
+                        prev_tup = tup
+                        if i == (len(self._begin_end_repeat_position) -1):
+                            # last tup : append to result
+                            begin_end_repeat_position_merge.append((b,e))
+                    else:
+                        # real end of repeat : append result and update b, e
+                        e = prev_tup[1]
+                        begin_end_repeat_position_merge.append((b,e))
+                        prev_tup = tup
+                        b = prev_tup[0]
+
+            self._begin_end_repeat_position = begin_end_repeat_position_merge
+
+    def _get_do_merge(self):
+        return self._do_merge
+
+    def _set_do_merge(self, do_merge):
+        if not isinstance(do_merge, bool):
+            raise TypeError("do_merge must be boolean")
+        # if different
+        if do_merge != self._do_merge:
+            self._do_merge = do_merge
+            if self._do_merge:
+                # did not merge before, merge now
+                if self._begin_end_repeat_position is None:
+                    self._find_begin_end_repeats()
+            else:
+                # data is already merged : need to compute again to un-merge
+                self._find_begin_end_repeats(force=True)
+
+    do_merge = property(_get_do_merge,_set_do_merge)
+
+    def hist_length_repeats(self, bins=50, alpha=0.5, hold=False, fontsize=12,
+                        grid=True, label="",xlabel="Repeat length", ylabel="#"):
+        # check that user has set a threshold
+        if self._list_len_repeats is None:
+            self._get_list_len_repeats()
+
+        if hold is False:
+            pylab.clf()
+        pylab.hist(self._list_len_repeats, alpha=alpha, label=label, bins=bins)
+        pylab.legend()
+        pylab.xlabel(xlabel, fontsize=fontsize)
+        pylab.ylabel(ylabel, fontsize=fontsize)
+        if grid is True:
+            pylab.grid(True)
 
