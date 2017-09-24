ce75e9fe72ad1117894d06a282512d4b6120d05b
Thomas Cokelaer <cokelaer@gmail.com>
2017-09-24 17:06:01 +0200

    add tests for the new atropos report parsing

--- test/modules_report/test_cutadapt.py
+++ test/modules_report/test_cutadapt.py
@@ -21,12 +21,25 @@ def test_output():
     assert mod.jinja['paired_reads_kept'] == '2,189'
 
     # atropos results should be identical except for a few differences (e.g.
-    # command starts with atropos)
-    # Note however that here, we used Nextera (I believe)
+    # command starts with atropos) This is for atropos 1.0.23 (txt only)
+    # Note that here, we used Nextera 
     filename = sequana_data("test_atropos_paired.txt")
     mod = CutadaptModule(filename, "sample_name")
-    assert mod.jinja['command'].startswith("atropos")
-    assert mod.jinja['mode'] == 'Paired-end'
     assert mod.jinja['paired_reads1_with_adapters'] == '197'
     assert mod.jinja['paired_reads2_with_adapters'] == '262'
     assert mod.jinja['paired_reads_kept'] == '2,420'
+
+
+
+def test_atropos_paired(tmpdir):
+    # This is for the new version of cutadapt with version 1.1
+    directory = tmpdir.mkdir('test_module')
+    config.output_dir = str(directory)
+    config.sample_name = 'JB409847'
+    c = CutadaptModule(sequana_data('test_atropos_pe.txt'), "TEST", "test.html")
+    assert c.jinja['mode'] == 'Paired-end'
+    c = CutadaptModule(sequana_data('test_atropos_se.txt'), "TEST", "test.html")
+    assert c.jinja['mode'] == 'Singled-end'
+
+
+
