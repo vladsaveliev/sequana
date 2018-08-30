09cf5c1c8d32ec2d60216da85d554e5f585922db
Thomas Cokelaer <cokelaer@gmail.com>
2018-08-31 00:57:48 +0200

    add bam_splitter tool

--- setup.py
+++ setup.py
@@ -128,7 +128,8 @@ setup(
            'sequana_mapping=sequana.scripts.mapping:main',
            'sequana_compressor=sequana.scripts.compressor:main',
            'sequana_report=sequana.scripts.reports:main',
-           'sequana_vcf_filter=sequana.scripts.vcf_filter:main',
+           'sequana_vcf_filter=sequana.scripts.vcf_filter:main', # june 2018
+           'sequana_bam_splitter=sequana.scripts.bam_splitter:main', # aug 2018
         ],
         'sequana.module':[
             'sequana_coverage=sequana.modules_report.coverage:CoverageModule',
