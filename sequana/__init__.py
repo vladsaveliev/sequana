8ab6eb6cc86fdb7e7af366a0acccd6d02c901e37
Thomas Cokelaer <cokelaer@gmail.com>
2018-09-05 08:02:42 +0200

    add SAM/BAM/CRAM in the API

--- sequana/__init__.py
+++ sequana/__init__.py
@@ -24,7 +24,7 @@ from .datatools import sequana_data
 
 from .assembly import *
 from .adapters import AdapterReader, FindAdaptersFromDesign, Adapter
-from .bamtools import BAM, SAMFlags
+from .bamtools import BAM, SAMFlags, SAM, CRAM
 from .bedtools import GenomeCov
 from .cigar import Cigar
 from .coverage import Coverage
